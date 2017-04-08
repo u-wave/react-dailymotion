bc63dd3031b3eb6deae432e0c31b1e3eb4baf200
René Kooi <renee@kooi.me>
2017-04-08 23:24:51 +0200

    Use prop-types.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import eventNames from './eventNames';
 import loadSdk from './loadSdk';
 
@@ -10,103 +10,103 @@ class Dailymotion extends React.Component {
      * public-accessible videos or kID (e.g. kABCD1234) for private-accessible
      * videos.
      */
-    video: React.PropTypes.string,
+    video: PropTypes.string,
     /**
      * DOM ID for the player element.
      */
-    id: React.PropTypes.string,
+    id: PropTypes.string,
     /**
      * CSS className for the player element.
      */
-    className: React.PropTypes.string,
+    className: PropTypes.string,
     /**
      * Width of the player element.
      */
-    width: React.PropTypes.oneOfType([
-      React.PropTypes.number,
-      React.PropTypes.string,
+    width: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
     ]),
     /**
      * Height of the player element.
      */
-    height: React.PropTypes.oneOfType([
-      React.PropTypes.number,
-      React.PropTypes.string,
+    height: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
     ]),
 
     /**
      * Pause the video.
      */
-    paused: React.PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
+    paused: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
 
     // Player parameters
 
     /**
      * Starts the playback of the video automatically after the player loads.
      */
-    autoplay: React.PropTypes.bool,
+    autoplay: PropTypes.bool,
     /**
      * Whether to display the player controls or not. This parameter only
      * removes the control bar, but keeps the startscreen and the endscreen
      * (useful on mobile devices where the video tag needs a direct user
      * interaction to start the playback).
      */
-    controls: React.PropTypes.bool,
+    controls: PropTypes.bool,
     /**
      * Whether to enable the end screen or not.
      */
-    showEndScreen: React.PropTypes.bool,
+    showEndScreen: PropTypes.bool,
     /**
      * Whether to mute the video or not.
      */
-    mute: React.PropTypes.bool,
+    mute: PropTypes.bool,
     /**
      * The domain of the page hosting the Dailymotion player. You might want to
      * specify origin for extra security.
      */
-    origin: React.PropTypes.string,
+    origin: PropTypes.string,
     /**
      * Specifies the _suggested_ playback quality for the video.
      */
-    quality: React.PropTypes.oneOf(['240', '380', '480', '720', '1080', '1440', '2160']),
+    quality: PropTypes.oneOf(['240', '380', '480', '720', '1080', '1440', '2160']),
     /**
      * Whether to display the sharing button or not.
      */
-    sharing: React.PropTypes.bool,
+    sharing: PropTypes.bool,
     /**
      * Specifies the time (in seconds) from which the video should start
      * playing.
      */
-    start: React.PropTypes.number,
+    start: PropTypes.number,
     /**
      * Specifies the selected subtitles language.
      */
-    subtitles: React.PropTypes.string,
+    subtitles: PropTypes.string,
     /**
      * Passes your syndication key to the player.
      */
-    syndication: React.PropTypes.string,
+    syndication: PropTypes.string,
     /**
      * Change the default highlight colour used in the controls (hex value).
      * See [the player customisation section](https://developer.dailymotion.com/player#player-customisation)
      * in the Dailymotion docs for
      * more on how this option is actually used.
      */
-    uiHighlightColor: React.PropTypes.string,
+    uiHighlightColor: PropTypes.string,
     /**
      * Whether to display the Dailymotion logo or not.
      */
-    uiShowLogo: React.PropTypes.bool,
+    uiShowLogo: PropTypes.bool,
     /**
      * Whether to show video information (title and owner) on the start screen.
      */
-    uiShowStartScreenInfo: React.PropTypes.bool,
+    uiShowStartScreenInfo: PropTypes.bool,
     /**
      * Choose the default base colour theme. See [the player customisation
      * section](https://developer.dailymotion.com/player#player-customisation)
      * in the Dailymotion docs for more on how this option is actually used.
      */
-    uiTheme: React.PropTypes.oneOf(['light', 'dark']),
+    uiTheme: PropTypes.oneOf(['light', 'dark']),
 
     // Player properties (not parameters, can only be set using methods)
 
@@ -114,7 +114,7 @@ class Dailymotion extends React.Component {
      * Sets the player's volume to the specified level, a number between 0 and
      * 1.
      */
-    volume: React.PropTypes.number,
+    volume: PropTypes.number,
 
     // Events
     /* eslint-disable react/no-unused-prop-types */
@@ -122,117 +122,117 @@ class Dailymotion extends React.Component {
     /**
      * Sent when the player reaches the end of an Ad media resource.
      */
-    onAdEnd: React.PropTypes.func,
+    onAdEnd: PropTypes.func,
     /**
      * Sent when an Ad playback pauses.
      */
-    onAdPause: React.PropTypes.func,
+    onAdPause: PropTypes.func,
     /**
      * Sent when an Ad playback starts.
      */
-    onAdPlay: React.PropTypes.func,
+    onAdPlay: PropTypes.func,
     /**
      * Sent when the player starts to play an Ad media resource.
      */
-    onAdStart: React.PropTypes.func,
+    onAdStart: PropTypes.func,
     /**
      * Sent on each Ad's time update.
      */
-    onAdTimeUpdate: React.PropTypes.func,
+    onAdTimeUpdate: PropTypes.func,
     /**
      * Sent when the player is ready to accept API commands.
      */
-    onApiReady: React.PropTypes.func,
+    onApiReady: PropTypes.func,
     /**
      * Sent when the duration of the video become available or change during
      * playback.
      */
-    onDurationChange: React.PropTypes.func,
+    onDurationChange: PropTypes.func,
     /**
      * Sent when playback has stopped at the end of the media resources set
      * (ads + content).
      */
-    onEnd: React.PropTypes.func,
+    onEnd: PropTypes.func,
     /**
      * Sent when the player triggers an error.
      */
-    onError: React.PropTypes.func,
+    onError: PropTypes.func,
     /**
      * Sent when the player enters or exits fullscreen.
      */
-    onFullscreenChange: React.PropTypes.func,
+    onFullscreenChange: PropTypes.func,
     /**
      * Sent when video's metadata are available.
      */
-    onLoadedMetadata: React.PropTypes.func,
+    onLoadedMetadata: PropTypes.func,
     /**
      * Sent when playback pauses after the pause method returns.
      */
-    onPause: React.PropTypes.func,
+    onPause: PropTypes.func,
     /**
      * Sent when playback starts after the `play` method returns.
      */
-    onPlay: React.PropTypes.func,
+    onPlay: PropTypes.func,
     /**
      * Sent when the content media resource playback has started.
      */
-    onPlaying: React.PropTypes.func,
+    onPlaying: PropTypes.func,
     /**
      * Sent when the browser is fetching the media data.
      */
-    onProgress: React.PropTypes.func,
+    onProgress: PropTypes.func,
     /**
      * Sent when qualities are available – see `qualities` for accepted values.
      */
-    onQualitiesAvailable: React.PropTypes.func,
+    onQualitiesAvailable: PropTypes.func,
     /**
      * Sent when the current quality changes.
      */
-    onQualityChange: React.PropTypes.func,
+    onQualityChange: PropTypes.func,
     /**
      * Sent when the player has completed a seeking operation.
      */
-    onSeeked: React.PropTypes.func,
+    onSeeked: PropTypes.func,
     /**
      * Sent when the player is starting to seek to another position in the video.
      */
-    onSeeking: React.PropTypes.func,
+    onSeeking: PropTypes.func,
     /**
      * Sent when the current subtitle changes.
      */
-    onSubtitleChange: React.PropTypes.func,
+    onSubtitleChange: PropTypes.func,
     /**
      * Sent when subtitles are available.
      */
-    onSubtitlesAvailable: React.PropTypes.func,
+    onSubtitlesAvailable: PropTypes.func,
     /**
      * Sent the first time the player attempts to start the playback, either
      * because of a user interaction, an autoplay parameter, or an API call
      * (e.g play(), load(), etc.).
      */
-    onStart: React.PropTypes.func,
+    onStart: PropTypes.func,
     /**
      * Sent when the playback position changes as part of normal playback or
      * because of some other condition.
      */
-    onTimeUpdate: React.PropTypes.func,
+    onTimeUpdate: PropTypes.func,
     /**
      * Sent when the player starts to play the content media resource.
      */
-    onVideoStart: React.PropTypes.func,
+    onVideoStart: PropTypes.func,
     /**
      * Sent when the player reaches the end of the content media resource.
      */
-    onVideoEnd: React.PropTypes.func,
+    onVideoEnd: PropTypes.func,
     /**
      * Sent when the player volume or mute state has changed.
      */
-    onVolumeChange: React.PropTypes.func,
+    onVolumeChange: PropTypes.func,
     /**
      * Sent when the player has to stop video playback for further buffering of
      * content.
      */
-    onWaiting: React.PropTypes.func,
+    onWaiting: PropTypes.func,
 
     /* eslint-enable react/no-unused-prop-types */
   };
