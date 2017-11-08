624a56087092d4e8c5c3edf86bf82daae4f78c02
Renée Kooi <renee@kooi.me>
2017-11-08 09:59:23 +0100

    Use scoped babel packages, update rollup-plugin-babel

--- .babelrc.js
+++ .babelrc.js
@@ -2,21 +2,15 @@ const rollup = process.env.ROLLUP === '1';
 const tests = process.env.TESTS === '1';
 
 const modules = rollup ? false : 'commonjs';
-const plugins = [];
-
-if (rollup) {
-  plugins.push('external-helpers');
-}
 
 module.exports = {
   presets: [
-    ['env', {
+    ['@babel/env', {
       loose: true,
       modules,
       targets: tests ? { node: 'current' } : {},
       forceAllTransforms: rollup,
     }],
-    'react',
+    '@babel/react',
   ],
-  plugins,
 };
